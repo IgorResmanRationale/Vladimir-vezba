f3574e9bf93123c2ad732578ac8a2e6c403e1c77
igor_resman <igor.resman@rationaletech.com>
2022-02-18 08:08:47 +0100

    zadatak1

--- Vladimir-vezba/src/app/app.component.ts
+++ Vladimir-vezba/src/app/app.component.ts
@@ -6,5 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Vladimir-vezba';
+  title = 'vladimir-vezba';
+  username = '';
+  password = '';
+  isLoginButtonClicked = false;
+  usernameArr: any = [];
+
+  onLogin(){
+    this.isLoginButtonClicked = true;
+    for(let i = 0; i < this.username.length; i++)
+    {
+      console.log(this.username[i])
+    }
+    //Dodatak
+    alert("Ulogovao si se");
+  }
+
+  onUsernameChange()
+  {
+    this.usernameArr = [];
+    for(let i = 0; i < this.username.length; i++)
+    {
+      this.usernameArr.push(this.username[i]);
+    }
+  }
+
+  // username = '';
+  // password = '';
+  // showSecret = false;
+  
+
+  // btnLogin(){
+  //   this.showSecret=!this.showSecret;
+  //   this.username.split('');
+  // }
 }
