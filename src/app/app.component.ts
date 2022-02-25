0dc7ec69f9248b0a736be407b45727c22fd8de22
igor_resman <igor.resman@rationaletech.com>
2022-02-25 13:49:21 +0100

    Igor_resenje_zadatka_2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'vladimir-vezba';
+  @ViewChild('signUpChild') signUpChild! : SignUpComponent;
+  showValidationMessage(message : string)
+  {
+
+    if(this.signUpChild != null && !this.signUpChild.areFieldsValid)
+    {
+      //dodatak
+      alert(message);
+      //nije trebalo ovako da se ispise nego u html ali je prikazano da bi se videlo da je moguce stilizovati poruke u konzoli
+      console.log('%c' + message , 'color:red')
+    }
+    if(this.signUpChild != null && this.signUpChild.areFieldsValid)
+    {
+      //dodatak
+      alert('User successfully registered');
+      console.log('%cUser successfully created' , 'color:green')
+    }
+  }
 }
