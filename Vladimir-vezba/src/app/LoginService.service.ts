02353bd5d603f51a6d0b1648c6383800b053a324
Vladimir <vladimir.skokin@rationaletech.com>
2022-05-26 13:05:23 +0200

    loginservice

--- Vladimir-vezba/src/app/LoginService.service.ts
+++ Vladimir-vezba/src/app/LoginService.service.ts
@@ -3,41 +3,38 @@ import { PreloadAllModules } from '@angular/router';
 import { EmployeeServiceService } from './employee-service.service';
 import { Account } from './models/employe.model';
 import { Router } from '@angular/router';
-import { debounceTime, Observable, scan, startWith } from 'rxjs';
-import { FormControl } from '@angular/forms';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginServiceService {
-  validation: Observable<string[]> | undefined ;
-  textControl = new FormControl();
   constructor(private service: EmployeeServiceService, private router: Router) { }
 
   getAccounts() {
     return this.service.accounts
   }
 
-  onSingIn(username: string, password: string,) {
-    const accounts = this.getAccounts();
-    const acc = accounts.find(x => x.username === username)
-    this.validation = this.textControl.valueChanges.pipe(
-      debounceTime(500), 
-      startWith(username), 
-      scan((acc, t) => t ? acc.concat(t) : [], [])
-    );
-    console.log(this.validation.pipe)
-    if (acc != null && acc.password === password && acc.username.length === +this.validation ) {
-      this.router.navigate(['/home']);
-      sessionStorage.setItem('Id', acc.id.toString())
-      sessionStorage.setItem('FullName', acc.name + ' ' + acc.lastName);
-      sessionStorage.setItem('Permission', acc.profile.permission);
-      alert('Korisnik uspesno ulogovan')
-      return;
-    }
-    else
-      alert('Korisnik ne postoji u sistemu')
-  }
+  // onSingIn(username: string, password: string,) {
+  //   const accounts = this.getAccounts();
+  //   const acc = accounts.find(x => x.username === username)
+  //   this.validation = this.textControl.valueChanges.pipe(
+  //     debounceTime(500), 
+  //     startWith(username), 
+  //     scan((acc, t) => t ? acc.concat(t) : [], [])
+  //   );
+  //   console.log(this.validation.pipe)
+  //   if (acc != null && acc.password === password && acc.username.length === +this.validation ) {
+  //     this.router.navigate(['/home']);
+  //     sessionStorage.setItem('Id', acc.id.toString())
+  //     sessionStorage.setItem('FullName', acc.name + ' ' + acc.lastName);
+  //     sessionStorage.setItem('Permission', acc.profile.permission);
+  //     alert('Korisnik uspesno ulogovan')
+  //     return;
+  //   }
+  //   else
+  //     alert('Korisnik ne postoji u sistemu')
+  // }
 
 
 }
